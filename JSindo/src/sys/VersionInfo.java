2e82bc6c29488d4cdf9bedb5fceb13467a13d9c6
Kiyoshi Yagi <kiyoshi.yagi@riken.jp>
2018-02-18 17:17:11 +0900

    Change version

--- JSindo/src/sys/VersionInfo.java
+++ JSindo/src/sys/VersionInfo.java
@@ -2,8 +2,8 @@ package sys;
 
 public class VersionInfo {
 
-   public static String ID      = "3.7";
+   public static String ID      = "4.0";
    public static String RELEASE = "develop";
-   public static String YEAR    = "2015";
+   public static String YEAR    = "2018";
    
 }
