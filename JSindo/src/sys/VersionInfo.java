51f199127bb251be1584f6c53808bbe9c74655c9
Kiyoshi Yagi <kiyoshi.yagi@riken.jp>
2018-04-14 14:22:36 +0900

    created documents for JSindo, rename jar file to 4.0.beta

--- JSindo/src/sys/VersionInfo.java
+++ JSindo/src/sys/VersionInfo.java
@@ -3,7 +3,7 @@ package sys;
 public class VersionInfo {
 
    public static String ID      = "4.0";
-   public static String RELEASE = "develop";
+   public static String RELEASE = "beta";
    public static String YEAR    = "2018";
    
 }
