e22ebb3c395fc515272a93a9c8e715db732b1409
Kiyoshi Yagi <kiyoshi.yagi@riken.jp>
2018-04-19 00:58:24 +0900

    modified L&F

--- JSindo/src/JSindo.java
+++ JSindo/src/JSindo.java
@@ -1,9 +1,30 @@
 
 import gui.*;
+import javax.swing.*;
 
 public class JSindo {
 
    public static void main(String[] args) {
+
+      // Setup look and feel for Swing
+      UIManager.LookAndFeelInfo[] lafInfo =UIManager.getInstalledLookAndFeels();
+      int nn=0;
+      for(int n=0; n<lafInfo.length; n++) {
+         String lafClassName = lafInfo[n].getClassName();
+         if(lafClassName.contains("nimbus")) {
+            nn = n;
+         }else if(lafClassName.contains("apple")) {
+            nn = n;
+            break;
+         }
+      }
+      try {
+         String lafClassName = lafInfo[nn].getClassName();
+         //System.out.println("Set L&F to "+lafClassName);
+         UIManager.setLookAndFeel(lafClassName);
+      }catch(Exception ex) {
+         System.out.println("Error L&F Setting");
+      }            
 
       GUIData guiData = new GUIData();
       @SuppressWarnings("unused")
