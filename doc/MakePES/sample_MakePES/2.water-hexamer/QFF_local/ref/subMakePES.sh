f361b43ed1e920b378a720b5ea248862e9e1ac27
Kiyoshi Yagi <kiyoshi.yagi@riken.jp>
2019-02-14 20:23:17 +0900

    added sample files MakePES

---
+++ doc/MakePES/sample_MakePES/2.water-hexamer/QFF_local/ref/subMakePES.sh
@@ -0,0 +1,29 @@
+######################################################
+# Batch file for a Sindo job (SMP Parallel)          #
+#
+#$ -S /bin/bash
+#$ -V
+#$ -cwd
+#$ -N makeQFF
+#$ -j y
+#$ -pe ompi 32
+#$ -q y4.q
+######################################################
+
+    if [ -e resources.info ]; then
+       rm resources.info
+    fi
+    touch resources.info
+
+    FILE=$(cat $PE_HOSTFILE | awk '{print $1}')
+    #cat $PE_HOSTFILE
+
+    for ihost in ${FILE[@]}; do
+       echo "$ihost" >> resources.info
+       echo "$ihost" >> resources.info
+    done
+
+    java RunMakePES -f makePES2.xml --input-version 2 >& makePES2.out
+
+    exit 0
+#
