d9485342a8a7e487aeb0e7da38c6601360c3a586
Kiyoshi Yagi <kiyoshi.yagi@riken.jp>
2019-04-28 14:38:35 +0900

    modified opt GeomOpt

--- JSindo/src/qchem/GaussianGeomOpt.java
+++ JSindo/src/qchem/GaussianGeomOpt.java
@@ -13,7 +13,9 @@ public class GaussianGeomOpt {
    private Molecule molecule = null;
    private ArrayList<double[][]> traj_min;
    private boolean force_isConv;
-   private int ngeom, nstep=-1;
+   private int niter, nstep;
+   private ArrayList<Double>   scf_energy;
+   private ArrayList<double[][]> td_info;
    private ArrayList<double[]> forceList;
    private ArrayList<double[]> mulliken_charge;
    private ArrayList<double[]> mulliken_spin;
@@ -22,24 +24,22 @@ public class GaussianGeomOpt {
       this.basename = basename;
    }
    
-   public int getNumOfIteration() {
-      return traj_min.size();
-   }
-   
    public void setNstep(int n) {
-      nstep = n;
+      nstep = n-1;
       return;
    }
    
    public Molecule readOutput() throws IOException {
       
-      molecule = new Molecule();
-      traj_min = new ArrayList<double[][]>();
+      molecule     = new Molecule();
+      traj_min     = new ArrayList<double[][]>();
+      scf_energy   = new ArrayList<Double>();
+      td_info      = new ArrayList<double[][]>();
       force_isConv = false;
-      forceList = new ArrayList<double[]>();
+      forceList    = new ArrayList<double[]>();
       mulliken_charge = new ArrayList<double[]>();
       mulliken_spin   = new ArrayList<double[]>();
-      
+      nstep = -1;
       
       BufferedReader br = new BufferedReader(new FileReader(basename+".out"));
       
@@ -88,9 +88,10 @@ public class GaussianGeomOpt {
          current_xyz[n] = xyz;
          
       }
-      
-      ngeom = 0;
+
+      niter = 0;
       int nn = 0;
+      double current_scf_energy = 0.0d;
       double rmsforce = 100.0;
       
       while((line = br.readLine()) != null){
@@ -120,13 +121,34 @@ public class GaussianGeomOpt {
             
          }
 
+         if(line.indexOf("SCF Done") != -1) {
+            String[] ss = Utilities.splitWithSpaceString(line);
+            current_scf_energy = Double.parseDouble(ss[4]);
+            scf_energy.add(Double.parseDouble(ss[4]));
+         }
+         
+         if(line.indexOf("Excitation energies") != -1) {
+            ArrayList<double[]> data = new ArrayList<double[]>();
+            while ((line = br.readLine()).indexOf("Leave Link") == -1) {
+               if(line.indexOf("Excited State") != -1) {
+                  String[] ss = Utilities.splitWithSpaceString(line);
+                  double[] dd = new double[2];
+                  dd[0] = Double.parseDouble(ss[4])/Constants.Hartree2eV + current_scf_energy;
+                  dd[1] = Double.parseDouble(ss[8].substring(2));
+                  data.add(dd);
+               }
+            }
+            double[][] e_and_f = data.toArray(new double[0][0]);
+            td_info.add(e_and_f);
+         }
+         
          if(line.indexOf("Maximum Force") != -1){
             String[] s1 = Utilities.splitWithSpaceString(line);
             String[] s2 = Utilities.splitWithSpaceString(br.readLine());
 
             if(! force_isConv && s1[4].equals("YES") && s2[4].equals("YES")){
                force_isConv = true;
-               ngeom = nn;
+               niter = nn;
             }
 
             double[] ff = new double[2];
@@ -144,7 +166,7 @@ public class GaussianGeomOpt {
             
             if(ff[1] < rmsforce){
                rmsforce = ff[1];
-               if(! force_isConv) ngeom = nn;
+               if(! force_isConv) niter = nn;
             }
             
             nn++;
@@ -175,7 +197,8 @@ public class GaussianGeomOpt {
          }
 
          if(line.indexOf("Normal termination") != -1){
-            ngeom = nn-1;
+            niter = nn-1;
+            nstep = niter;
          }
 
          
@@ -183,17 +206,26 @@ public class GaussianGeomOpt {
       
       br.close();
       
+      // Remove the last duplicate structures
+      traj_min.remove(traj_min.size()-1);
       return molecule;
       
    }
    
    /**
-    * Returns the molecule 
+    * Returns the number of iteration for optimization
+    * @return The number of iteration
+    */
+   public int getNumOfIteration() {
+      return niter+1;
+   }
+
+   /**
+    * Returns the molecule at the current step
     * @return the molecule
     */
    public Molecule getMolecule(){
       if(molecule != null){
-         if(nstep < 0) nstep = ngeom;
          for(int n=0; n<molecule.getNat(); n++){
             molecule.getAtom(n).setXYZCoordinates(traj_min.get(nstep)[n]);
          }
@@ -201,31 +233,122 @@ public class GaussianGeomOpt {
       return this.molecule;
    }
    
+   /**
+    * Returns the Mulliken charge at the last step
+    * @return atomic charge (double[natom])
+    */
+   public double[] getCharge(){
+      if(mulliken_charge.size() > 0){         
+         return mulliken_charge.get(nstep);
+      }else{
+         return null;
+      }
+   }
+
+   /**
+    * Returns the spin density at the last step
+    * @return spin density (double[natom])
+    */
+   public double[] getSpin(){
+      if(mulliken_spin.size() > 0){
+         return mulliken_spin.get(nstep);         
+      }else{
+         return null;
+      }
+   }
+   
+   /**
+    * Returns the SCF energy at the current step
+    * @return An arraylist of scf energy
+    */
+   public double getSCF_energy(){
+      return scf_energy.get(nstep);
+   }
+
+   /**
+    * Returns the information of TD-HF/DFT at the current step
+    * @return double[nstate][energy, oscillator strength]
+    */
+   public double[][] getTD(){
+      if(td_info.size() > 0) {
+         return td_info.get(nstep);         
+      }else {
+         return null;
+      }
+   }
+
+   /**
+    * Returns the force at the current step
+    * @return double[0]=maxforce, double[1]=rmsforce
+    */
+   public double[] getForce(){
+      return forceList.get(nstep);
+   }
+
+   /**
+    * Returns the molecule at the last step
+    * @return the molecule
+    */
    public Molecule getLastMolecule(){
       
       if(molecule != null){
-         int last = traj_min.size()-1;
          for(int n=0; n<molecule.getNat(); n++){
-            molecule.getAtom(n).setXYZCoordinates(traj_min.get(last)[n]);
+            molecule.getAtom(n).setXYZCoordinates(traj_min.get(niter)[n]);
          }         
       }
       return this.molecule;
    }
    
+   /**
+    * Returns the Mulliken charge at the last step
+    * @return atomic charge (double[natom])
+    */
    public double[] getLastCharge(){
       if(mulliken_charge.size() > 0){         
-         return mulliken_charge.get(mulliken_charge.size()-1);
+         return mulliken_charge.get(niter);
+      }else{
+         return null;
+      }
+   }
+
+   /**
+    * Returns the spin density at the last step
+    * @return spin density (double[natom])
+    */
+   public double[] getLastSpin(){
+      if(mulliken_spin.size() > 0){
+         return mulliken_spin.get(niter);         
       }else{
          return null;
       }
    }
    
-   public double[] getLastSpin(){
-      if(mulliken_spin.size() > 0){
-         return mulliken_spin.get(mulliken_spin.size()-1);         
-      }else{
+   /**
+    * Returns the SCF energy at the last step
+    * @return the SCF energy
+    */
+   public double getLastSCF_energy() {
+      return scf_energy.get(niter);
+   }
+   
+   /**
+    * Returns the TD-HF/DFT information at the last step
+    * @return the TD info
+    */
+   public double[][] getLastTD() {
+      if(td_info.size() > 0) {
+         return td_info.get(niter);
+      }else {
          return null;
       }
+   }
+
+   /**
+    * Returns the force at the last step
+    * @return double[nstate][energy, oscillator strength]
+    */
+   public double[] getLastForce() {
+      return forceList.get(niter);
    }
    
    /**
@@ -234,14 +357,6 @@ public class GaussianGeomOpt {
     */
    public ArrayList<double[][]> getTrajectory(){
       return traj_min;
-   }
-   
-   /**
-    * Returns the force at each step
-    * @return An arraylist of force (double[0]=maxforce, double[1]=rmsforce)
-    */
-   public ArrayList<double[]> getForce(){
-      return forceList;
    }
    
    /**
@@ -254,20 +369,20 @@ public class GaussianGeomOpt {
    
    public void printStatus(){
 
-      double[] ff = forceList.get(ngeom);
+      double[] ff = forceList.get(niter);
       double maxforce = ff[0];
       double rmsforce = ff[1];
       
-      if(ngeom == 0){
+      if(niter == 0){
          System.out.println("The optimization is terminated at the first geometry...");
          System.out.printf(" maxforce = %12.6f  (G09threshold=0.000450) \n",maxforce);
          System.out.printf(" rmsforce = %12.6f  (G09threshold=0.000300) \n",rmsforce);
       }else if(force_isConv){
-         System.out.printf("The force is converged at the %3d-th step \n",ngeom+1);
+         System.out.printf("The force is converged at the %3d-th step \n",niter+1);
          System.out.printf(" maxforce = %12.6f  (G09threshold=0.000450) \n",maxforce);
          System.out.printf(" rmsforce = %12.6f  (G09threshold=0.000300) \n",rmsforce);
       }else {
-         System.out.printf("The next geometry is taken from the %3d-th step \n",ngeom+1);
+         System.out.printf("The next geometry is taken from the %3d-th step \n",niter+1);
          System.out.printf(" maxforce = %12.6f  (G09threshold=0.000450) \n",maxforce);
          System.out.printf(" rmsforce = %12.6f  (G09threshold=0.000300) \n",rmsforce);
       }
