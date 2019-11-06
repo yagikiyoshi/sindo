ff51d2c2a8ee43d0b9b34cb799d10c417753b3f6
Kiyoshi Yagi <kiyoshi.yagi@riken.jp>
2019-11-06 22:51:40 +0900

    calc Averate Temperature

--- JSindo/src/GetTemperature.java
+++ JSindo/src/GetTemperature.java
@@ -7,19 +7,26 @@ public class GetTemperature {
    public static void main(String[] args) {
       
       if (args.length == 0) {
-         System.out.println("Usage: java GetTemperature [--throw N] log1 log2 ");
+         System.out.println("Usage: java GetTemperature [--throw N] [--average 10] log1 log2 ");
          System.out.println(" log1 log2 : logfiles");
          System.out.println(" (optional) --throw N: Throw the first N steps for calculating the average and RMSD");
+         System.out.println(" (optional) --average N: Also obtain averaged value over N windows");
          System.exit(0);
       }
       
       int nthrow = 0;
+      int nave   = 0;
       String[] logname = new String[args.length];
       int ndata = 0;
       for(int n=0; n<args.length; n++) {
          if (args[n].equals("--throw")) {
             nthrow = Integer.parseInt(args[n+1]);
             n++;
+            
+         }else if (args[n].equals("--average")) {
+            nave = Integer.parseInt(args[n+1]);
+            n++;
+
          }else {
             logname[ndata] = args[n];
             ndata++;
@@ -51,9 +58,27 @@ public class GetTemperature {
          ave[n] = 0.D+00;
          try {
             PrintWriter pw = new PrintWriter(dat);
-            for(int i=0; i<time.length; i++) {
-               pw.printf("%12.4f  %15.4f \n", time[i], temperature[i]);
-               if (i > nthrow) ave[n] += temperature[i];
+            
+            if(nave > 0) {
+               for(int i=0; i<time.length; i++) {
+                  int ist = i - nave;
+                  if(ist < 0) ist = 0;
+                  Double tave = 0.0d;
+                  for(int j=ist; j<=i; j++) {
+                     tave += temperature[j];
+                  }
+                  tave = tave / (double)(i - ist +1);
+                  pw.printf("%12.4f  %15.4f   %15.4f \n", time[i], temperature[i], tave);
+                  if (i > nthrow) ave[n] += temperature[i];
+               }
+               
+               
+            }else {
+               for(int i=0; i<time.length; i++) {
+                  pw.printf("%12.4f  %15.4f \n", time[i], temperature[i]);
+                  if (i > nthrow) ave[n] += temperature[i];
+               }
+               
             }
             pw.close();
             
