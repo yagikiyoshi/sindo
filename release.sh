c198f55250ba6c37f0cf3c1a506ca2f4e94b4ad0
Kiyoshi Yagi <kiyoshi.yagi@riken.jp>
2020-09-20 23:40:24 +0900

    update release script

--- release.sh
+++ release.sh
@@ -5,7 +5,7 @@
 #  o FSindo/src/tools.f90
 #
 
-version=$(cat VERSION)
+version=$(cat VERSION)_$(date +%y%m%d)
 sindo=sindo-$version
 doc=doc-$version
 
@@ -18,8 +18,8 @@ docDir=$releaseDir/$doc
 
 echo "Creating a release version [ "$(date +%F)" ]"
 mkdir -p $sindoDir
-mkdir $sindoDir/jar
 mkdir $sindoDir/FSindo
+mkdir $sindoDir/JSindo
 mkdir -p $docDir
 mkdir $docDir/JSindo
 mkdir $docDir/FSindo
@@ -28,51 +28,54 @@ mkdir $docDir/GENESIS
 mkdir $docDir/lecture_notes
 
 # copy license and readme
-cp LICENSE $sindoDir
-cp README.md $sindoDir
+cp -a LICENSE $sindoDir
+cp -a README.md $sindoDir
+
+# copy environment variables
+cp -a sindovars.sh $sindoDir
 
 # JSindo
 cd JSindo
 ant -f build_JSindo.xml  >& /dev/null
-cp jar/JSindo*.jar $sindoDir/jar
+cp -a jar $sindoDir/JSindo
 cd ..
 
 # FSindo
 cd FSindo
 tarball=fsindo.tar.gz
-tar --exclude *.o --exclude *.mod --exclude *.inc -zcf $tarball configure config src util
+tar --exclude *.o --exclude *.mod --exclude make.inc -zcf $tarball configure config src util
 tar -zxf $tarball -C $sindoDir/FSindo
 rm $tarball
 cd ..
 
 # script
-cp -r script $sindoDir
+cp -a script $sindoDir
 rm $sindoDir/script/runGaussian.sh
 
 # doc
 cd doc/JSindo
-cp *.pdf  $docDir/JSindo
+cp -a *.pdf  $docDir/JSindo
 
 tar -zcf sample_JSindo.tar.gz sample_JSindo
 mv sample_JSindo.tar.gz $docDir/JSindo
 
 cd ../FSindo
-cp *.pdf  $docDir/FSindo
+cp -a *.pdf  $docDir/FSindo
 tar -zcf sample_FSindo.tar.gz sample_FSindo
 mv sample_FSindo.tar.gz $docDir/FSindo
 
 cd ../MakePES
-cp *.pdf  $docDir/MakePES
+cp -a *.pdf  $docDir/MakePES
 tar -zcf sample_MakePES.tar.gz sample_MakePES
 mv sample_MakePES.tar.gz $docDir/MakePES
 
 cd ../GENESIS
-cp *.pdf  $docDir/GENESIS
+cp -a *.pdf  $docDir/GENESIS
 tar -zcf sample_sindo_genesis.tar.gz --exclude *wako.sh sample_sindo_genesis
 mv sample_sindo_genesis.tar.gz $docDir/GENESIS
 
 cd ../lecture_notes
-cp *.pdf $docDir/lecture_notes
+cp -a *.pdf $docDir/lecture_notes
 
 
 # create archive
--- sindovars.sh
+++ sindovars.sh
@@ -1,5 +1,6 @@
 
-export sindo_dir=/path/to/sindo-4.0
+#export sindo_dir=/path/to/sindo-4.0
+export sindo_dir=~/Work/devel/sindo/sindo.master
 export sindo_jar=$sindo_dir/JSindo/jar
 export PATH=$PATH:$sindo_dir/script:$sindo_dir/FSindo/bin
 export SINDO_RSH=ssh
