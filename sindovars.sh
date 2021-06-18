29a1ee0c8c66abae64f3a186079a7b46f05c9d3b
Kiyoshi Yagi <kiyoshi.yagi@riken.jp>
2021-06-18 19:47:49 +0900

    set external jars

--- sindovars.sh
+++ sindovars.sh
@@ -1,6 +1,9 @@
 
-#export sindo_dir=/path/to/sindo-4.0
-export sindo_dir=~/Work/devel/sindo/sindo.master
+#export sindo_dir=/home1/yagi/devel/sindo
+export sindo_dir=/path/to/sindo-4.0
 export sindo_jar=$sindo_dir/JSindo/jar
+export CLASSPATH=${CLASSPATH}:${sindo_jar}/JSindo-4.0.beta.jar
+export CLASSPATH=${CLASSPATH}:${sindo_jar}/ext/Jama-1.0.3.jar
+
 export PATH=$PATH:$sindo_dir/script:$sindo_dir/FSindo/bin
 export SINDO_RSH=ssh
