5899217e9f59142d490ab7b10a56e4cd80d213f0
Kiyoshi Yagi <kiyoshi.yagi@chem.tsukuba.ac.jp>
2026-04-21 23:29:47 +0900

    update sindovars.sh

--- sindovars.sh
+++ sindovars.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-export sindo_dir=/path/to/sindo
-export CLASSPATH=${CLASSPATH}:$sindo_dir/JSindo/jar/JSindo-version_fat.jar
+export sindo_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)
+
+version=$(cat ${sindo_dir}/VERSION)
+export CLASSPATH=${CLASSPATH}:$sindo_dir/JSindo/jar/JSindo-${version}_fat.jar
+
 export PATH=$PATH:$sindo_dir/script:$sindo_dir/FSindo/bin
 export SINDO_RSH=ssh
